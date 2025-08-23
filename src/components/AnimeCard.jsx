6d647d413242ef51ddd32ce702d38151ab8488d0
anidock <anidockofficial@gmail.com>
2025-08-23 16:54:59 +0530

    Update AnimeCard.jsx

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+
+function AnimeCard({ title, img }) {
+  return (
+    <div className="bg-gray-700 rounded-lg overflow-hidden hover:scale-105 transition-transform">
+      <img src={img} alt={title} className="w-full h-48 object-cover" />
+      <div className="p-4">
+        <h3 className="text-xl font-semibold">{title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default AnimeCard;
 
