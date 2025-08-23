d5079b582eee4079374fcfa6c62033ec4c9e9955
anidock <anidockofficial@gmail.com>
2025-08-23 16:10:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // crucial for Vercel deployment
+  base: './', // <-- important for Vercel deployment
 });
